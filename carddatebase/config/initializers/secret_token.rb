b3b5d3dd782a63a084d8d5f2a0c987e156fdba87
wadror <wadror@wadror-VirtualBox.(none)>
2014-03-16 23:00:09 +0200

    Jotain

---
+++ carddatebase/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carddatebase::Application.config.secret_key_base = '51fb3de04be7be060207a32ad0e69bff698a67abe4cdaea23d17cb7e8f26ffec6c2b75c68e9d4489d19aebcb6afbd3befe35e19aa755da967d2771227913a619'
