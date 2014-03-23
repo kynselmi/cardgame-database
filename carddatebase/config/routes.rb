756b766d621c28f3014f74e8cb08798fd423c34c
wadror <wadror@wadror-VirtualBox.(none)>
2014-03-23 21:49:29 +0200

    Readme

--- carddatebase/config/routes.rb
+++ carddatebase/config/routes.rb
@@ -1,4 +1,14 @@
 Carddatebase::Application.routes.draw do
+  resources :heros
+
+  resources :decks
+
+  resources :cards
+
+  resources :users
+
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
