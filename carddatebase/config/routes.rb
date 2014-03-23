47faa930a9b74275502b39b6a1637179e4053589
wadror <wadror@wadror-VirtualBox.(none)>
2014-03-23 23:01:25 +0200

    Viikko 2 final

--- carddatebase/config/routes.rb
+++ carddatebase/config/routes.rb
@@ -7,7 +7,7 @@ Carddatebase::Application.routes.draw do
 
   resources :users
 
-  root 'users#index'
+  root 'cards#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
